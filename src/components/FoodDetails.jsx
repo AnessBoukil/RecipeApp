4ce309c8caf9688d4e7ac1fb9c1f3ba2f21695f8
aness_boukil <161384382+AnessBoukil@users.noreply.github.com>
2024-12-26 00:24:31 +0100

    Add files via upload

---
+++ src/components/FoodDetails.jsx
@@ -0,0 +1,3 @@
+export default function FoodDetails({foodId}){
+    return <div>food {foodId}</div>
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import styles from "./search.module.css";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+//place your own key from spoonacular.com here
+const API_KEY = "c1f0d5e121f9448299eb12b8923300db";
+
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza");
+  //useEffect hook syntax
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [query]);
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.input}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+}
