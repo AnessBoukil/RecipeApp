8857bec6e43f532eb2b75afa0571d75d32350a02
aness_boukil <161384382+AnessBoukil@users.noreply.github.com>
2024-12-26 01:40:17 +0100

    Add files via upload

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,3 +1,48 @@
-export default function FoodDetails({foodId}){
-    return <div>food {foodId}</div>
+import { useEffect, useState } from "react";
+
+export default function FoodDetails({ foodId }) {
+  const [food, setFood] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const url = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "c1f0d5e121f9448299eb12b8923300db";
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${url}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+      setIsLoading(false);
+    }
+    fetchFood();
+  }, [foodId]);
+  return (
+    <div>
+      <div>
+        <h1>{food.title}</h1>
+        <img src={food.image} alt="" />
+        <div></div>
+        <div>
+          <span>
+            <strong>⏱️{food.readyInMinutes} Minutes</strong>
+          </span>
+          <span>
+            <strong>👨‍👩‍👧‍👦Serves {food.servings}</strong>
+          </span>
+          <span>{food.vegetarian ? "Vegeterian" : "Non-vegeterian"}</span>
+          <span>{food.vegan ? "Vegan" : ""}</span>
+        </div>
+        <div>
+          Price : <span>{food.pricePerServing / 100}$</span>
+        </div>
+      </div>
+      <div>
+        <h2>Instructions</h2>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          food.analyzedInstructions[0].steps.map((step) => <li>{step.step}</li>)
+        )}
+      </div>
+    </div>
+  );
 }
